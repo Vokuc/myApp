e3ee388b0601d1b9590942d4956a72e282958a28
Chika Kalu Ukpaii <chikau@phreetech.com>
2023-04-21 05:55:28 +0100

    First commit in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from './housing-location';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myApp';
+  title = 'FAIRHOUSE';
+
+  housingLocationList: HousingLocation[] = [
+    {
+      name: "Presidential Villa",
+      city: "Eziobodo",
+      state: "Imo",
+      photo: "../assets/presidential.jpg",
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    },
+    {
+      name: "Glo Lodge",
+      city: "Eziobodo",
+      state: "Imo",
+      photo: "../assets/glo.jpg",
+      availableUnits: 1,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      name: "Dominion City",
+      city: "Eziobodo",
+      state: "Imo",
+      photo: "../assets/dcity.jpg",
+      availableUnits: 1,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      name: "Koko Mansion",
+      city: "Umuchima",
+      state: "Imo",
+      photo: "../assets/koko.jpg",
+      availableUnits: 7,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      name: "Fair Haven",
+      city: "Ihiagwa",
+      state: "Imo",
+      photo: "../assets/fair.jpg",
+      availableUnits: 1,
+      wifi: false,
+      laundry: true,
+    },
+  ]
 }
